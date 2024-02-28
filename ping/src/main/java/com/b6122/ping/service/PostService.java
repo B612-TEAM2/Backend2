0b6febc92cdc23e63cf8b2b47e913ef7a5551ccc
Hwan Jun Kim <spdlqjdkdlel1702@naver.com>
2024-02-28 17:06:04 +0900

    fix 이미지 null 처리

--- ping/src/main/java/com/b6122/ping/service/PostService.java
+++ ping/src/main/java/com/b6122/ping/service/PostService.java
@@ -43,7 +43,9 @@ public class PostService {
         post.setViewCount(postDto.getViewCount());
         post.setLikeCount(postDto.getLikeCount());
         post.setLikes(postDto.getLikes());
-        post.setPostImgObjectsName(imgs);
+        if (imgs != null) {
+            post.setPostImgObjectsName(imgs);
+        }
         return postRepository.save(post);
     }
 
