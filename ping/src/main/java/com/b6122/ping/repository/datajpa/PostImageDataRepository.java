b4c7d1346ee1d2f9a7e51854ce4c7f4fbe0f831b
Hwan Jun Kim <spdlqjdkdlel1702@naver.com>
2024-03-08 19:03:25 +0900

    feat PostImage 엔티티 생성

---
+++ ping/src/main/java/com/b6122/ping/repository/datajpa/PostImageDataRepository.java
@@ -0,0 +1,8 @@
+package com.b6122.ping.repository.datajpa;
+
+import com.b6122.ping.domain.PostImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostImageDataRepository extends JpaRepository<PostImage, Long> {
+
+}
